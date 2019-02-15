f08704a1ffa90e9d145f404dabfe7fbf69f1fc9d
driller <eleshis@gmail.com>
2019-02-15 14:38:50 +0900

    0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jpxtime3",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     package_data={"": ["*pickle4"]},
     description="Japan Exchange Delivatives(Index) trading schedule and trading hours",
